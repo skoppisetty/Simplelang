081080190341c5dfdefeffc337d2d820342b57f3
suresh <suresh.koppisetty@gmail.com>
2015-01-18 13:10:02 -0500

    Initial code

---
+++ src/cop5555sp15/Scanner.java
@@ -0,0 +1,22 @@
+package cop5555sp15;
+
+import cop5555sp15.TokenStream.Kind;
+import cop5555sp15.TokenStream.Token;
+import static cop5555sp15.TokenStream.Kind.*;
+
+public class Scanner {
+
+
+	public Scanner(TokenStream stream) {
+		//IMPLEMENT THIS
+	}
+
+
+	// Fills in the stream.tokens list with recognized tokens 
+     //from the input
+	public void scan() {
+         // IMPLEMENT THIS
+	}
+
+}
+
