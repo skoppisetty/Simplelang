7febef7e0e923b498d810a186e7f9e636e66a853
suresh <suresh.koppisetty@gmail.com>
2015-04-10 03:33:05 -0400

    Assignment 4 - 4 test cases pending

---
+++ src/cop5555sp15/ast/TypeCheckVisitor.java
@@ -0,0 +1,426 @@
+package cop5555sp15.ast;
+
+import static cop5555sp15.TokenStream.Kind.AND;
+import static cop5555sp15.TokenStream.Kind.BAR;
+import static cop5555sp15.TokenStream.Kind.DIV;
+import static cop5555sp15.TokenStream.Kind.EQUAL;
+import static cop5555sp15.TokenStream.Kind.GE;
+import static cop5555sp15.TokenStream.Kind.GT;
+import static cop5555sp15.TokenStream.Kind.LE;
+import static cop5555sp15.TokenStream.Kind.LSHIFT;
+import static cop5555sp15.TokenStream.Kind.LT;
+import static cop5555sp15.TokenStream.Kind.MINUS;
+import static cop5555sp15.TokenStream.Kind.NOTEQUAL;
+import static cop5555sp15.TokenStream.Kind.PLUS;
+import static cop5555sp15.TokenStream.Kind.RSHIFT;
+import static cop5555sp15.TokenStream.Kind.TIMES;
+
+import java.util.Arrays;
+
+import cop5555sp15.TypeConstants;
+import cop5555sp15.TokenStream.Kind;
+import cop5555sp15.symbolTable.SymbolTable;
+
+public class TypeCheckVisitor implements ASTVisitor, TypeConstants {
+
+	@SuppressWarnings("serial")
+	public static class TypeCheckException extends Exception {
+		ASTNode node;
+
+		public TypeCheckException(String message, ASTNode node) {
+			super(node.firstToken.lineNumber + ":" + message);
+			this.node = node;
+		}
+	}
+
+	SymbolTable symbolTable;
+	static final Kind[] REL_OPS = { BAR, AND, EQUAL, NOTEQUAL, LT, GT, LE, GE };
+	static final Kind[] WEAK_OPS = { PLUS, MINUS };
+	static final Kind[] STRONG_OPS = { TIMES, DIV };
+	static final Kind[] VERY_STRONG_OPS = { LSHIFT, RSHIFT };
+	
+	public TypeCheckVisitor(SymbolTable symbolTable) {
+		this.symbolTable = symbolTable;
+	}
+
+	boolean check(boolean condition, String message, ASTNode node)
+			throws TypeCheckException {
+		if (condition)
+			return true;
+		throw new TypeCheckException(message, node);
+	}
+
+	/**
+	 * Ensure that types on left and right hand side are compatible.
+	 */
+	@Override
+	public Object visitAssignmentStatement(
+			AssignmentStatement assignmentStatement, Object arg)
+			throws Exception {
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+
+	/**
+	 * Ensure that both types are the same, save and return the result type
+	 */
+	@Override
+	public Object visitBinaryExpression(BinaryExpression binaryExpression,
+			Object arg) throws Exception {
+		binaryExpression.expression0.visit(this, arg);
+		binaryExpression.expression1.visit(this, arg);
+//		System.out.println(binaryExpression.expression0.getType());
+//		System.out.println(binaryExpression.expression1.getType());
+		
+		if(binaryExpression.expression0.getType() == binaryExpression.expression1.getType()){
+			if(binaryExpression.expression0.getType() == intType){
+				if(Arrays.asList(WEAK_OPS).contains(binaryExpression.op.kind) ||
+				Arrays.asList(STRONG_OPS).contains(binaryExpression.op.kind)){
+					binaryExpression.setType(intType);
+					return intType;
+				}
+				else if(binaryExpression.op.kind == EQUAL || 
+						binaryExpression.op.kind == NOTEQUAL ||
+						binaryExpression.op.kind == LT ||
+						binaryExpression.op.kind == LE ||
+						binaryExpression.op.kind == GT ||
+						binaryExpression.op.kind == GE ){
+					binaryExpression.setType(booleanType);
+					return booleanType;
+				}
+				else{
+					throw new TypeCheckException("Int only supports +,-,* and /",binaryExpression);
+				}
+			}
+			if(binaryExpression.expression0.getType() == stringType){
+				if(binaryExpression.op.kind == PLUS){
+					binaryExpression.setType(stringType);
+					return stringType;
+				}
+				else if(binaryExpression.op.kind == EQUAL || binaryExpression.op.kind == NOTEQUAL){
+					binaryExpression.setType(booleanType);
+					return booleanType;
+				}
+				else{
+					throw new TypeCheckException("String only supports + , != and ==",binaryExpression);
+				}
+			}
+			if(binaryExpression.expression0.getType() == booleanType){
+				if(binaryExpression.op.kind == EQUAL || binaryExpression.op.kind == NOTEQUAL  ||
+						binaryExpression.op.kind == AND ||
+						binaryExpression.op.kind == BAR ){
+					binaryExpression.setType(booleanType);
+					return booleanType;
+				}
+				else{
+					throw new TypeCheckException("Boolean only supports != and ==",binaryExpression);
+				}
+			}
+			else{
+				throw new TypeCheckException("Unknown operand type",binaryExpression);
+			}
+			
+		}
+		else{
+			throw new TypeCheckException("Mismatch in types of expression ",binaryExpression);
+		}
+	}
+
+	/**
+	 * Blocks define scopes. Check that the scope nesting level is the same at
+	 * the end as at the beginning of block
+	 */
+	@Override
+	public Object visitBlock(Block block, Object arg) throws Exception {
+		int numScopes = symbolTable.enterScope();
+		// visit children
+		for (BlockElem elem : block.elems) {
+			elem.visit(this, arg);
+		}
+		int numScopesExit = symbolTable.leaveScope();
+		check(numScopesExit > 0 && numScopesExit == numScopes,
+				"unbalanced scopes", block);
+		return null;
+	}
+
+	/**
+	 * Sets the expressionType to booleanType and returns it
+	 * 
+	 * @param booleanLitExpression
+	 * @param arg
+	 * @return
+	 * @throws Exception
+	 */
+	@Override
+	public Object visitBooleanLitExpression(
+			BooleanLitExpression booleanLitExpression, Object arg)
+			throws Exception {
+		booleanLitExpression.setType(booleanType);
+		return booleanType;
+	}
+
+	/**
+	 * A closure defines a new scope Visit all the declarations in the
+	 * formalArgList, and all the statements in the statementList construct and
+	 * set the JVMType, the argType array, and the result type
+	 * 
+	 * @param closure
+	 * @param arg
+	 * @return
+	 * @throws Exception
+	 */
+	@Override
+	public Object visitClosure(Closure closure, Object arg) throws Exception {
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+
+	/**
+	 * Make sure that the name has not already been declared and insert in
+	 * symbol table. Visit the closure
+	 */
+	@Override
+	public Object visitClosureDec(ClosureDec closureDec, Object arg) {
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+
+	/**
+	 * Check that the given name is declared as a closure Check the argument
+	 * types The type is the return type of the closure
+	 */
+	@Override
+	public Object visitClosureEvalExpression(
+			ClosureEvalExpression closureExpression, Object arg)
+			throws Exception {
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+
+	@Override
+	public Object visitClosureExpression(ClosureExpression closureExpression,
+			Object arg) throws Exception {
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+
+	@Override
+	public Object visitExpressionLValue(ExpressionLValue expressionLValue,
+			Object arg) throws Exception {
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+
+	@Override
+	public Object visitExpressionStatement(
+			ExpressionStatement expressionStatement, Object arg)
+			throws Exception {
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+
+	/**
+	 * Check that name has been declared in scope Get its type from the
+	 * declaration.
+	 * 
+	 */
+	@Override
+	public Object visitIdentExpression(IdentExpression identExpression,
+			Object arg) throws Exception {
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+
+	@Override
+	public Object visitIdentLValue(IdentLValue identLValue, Object arg)
+			throws Exception {
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+
+	@Override
+	public Object visitIfElseStatement(IfElseStatement ifElseStatement,
+			Object arg) throws Exception {
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+
+	/**
+	 * expression type is boolean
+	 */
+	@Override
+	public Object visitIfStatement(IfStatement ifStatement, Object arg)
+			throws Exception {
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+
+	/**
+	 * expression type is int
+	 */
+	@Override
+	public Object visitIntLitExpression(IntLitExpression intLitExpression,
+			Object arg) throws Exception {
+		intLitExpression.setType(intType);
+		return intType;
+	}
+
+	@Override
+	public Object visitKeyExpression(KeyExpression keyExpression, Object arg)
+			throws Exception {
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+
+	@Override
+	public Object visitKeyValueExpression(
+			KeyValueExpression keyValueExpression, Object arg) throws Exception {
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+
+	@Override
+	public Object visitKeyValueType(KeyValueType keyValueType, Object arg)
+			throws Exception {
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+
+	// visit the expressions (children) and ensure they are the same type
+	// the return type is "Ljava/util/ArrayList<"+type0+">;" where type0 is the
+	// type of elements in the list
+	// this should handle lists of lists, and empty list. An empty list is
+	// indicated by "Ljava/util/ArrayList;".
+	@Override
+	public Object visitListExpression(ListExpression listExpression, Object arg)
+			throws Exception {
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+
+	/** gets the type from the enclosed expression */
+	@Override
+	public Object visitListOrMapElemExpression(
+			ListOrMapElemExpression listOrMapElemExpression, Object arg)
+			throws Exception {
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+
+	@Override
+	public Object visitListType(ListType listType, Object arg) throws Exception {
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+
+	@Override
+	public Object visitMapListExpression(MapListExpression mapListExpression,
+			Object arg) throws Exception {
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+
+	@Override
+	public Object visitPrintStatement(PrintStatement printStatement, Object arg)
+			throws Exception {
+		printStatement.expression.visit(this, null);
+		return null;
+	}
+
+	@Override
+	public Object visitProgram(Program program, Object arg) throws Exception {
+		if (arg == null) {
+			program.JVMName = program.name;
+		} else {
+			program.JVMName = arg + "/" + program.name;
+		}
+		// ignore the import statement
+		if (!symbolTable.insert(program.name, null)) {
+			throw new TypeCheckException("name already in symbol table",
+					program);
+		}
+		program.block.visit(this, true);
+		return null;
+	}
+
+	@Override
+	public Object visitQualifiedName(QualifiedName qualifiedName, Object arg) {
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * Checks that both expressions have type int.
+	 * 
+	 * Note that in spite of the name, this is not in the Expression type
+	 * hierarchy.
+	 */
+	@Override
+	public Object visitRangeExpression(RangeExpression rangeExpression,
+			Object arg) throws Exception {
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+
+	// nothing to do here
+	@Override
+	public Object visitReturnStatement(ReturnStatement returnStatement,
+			Object arg) throws Exception {
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+
+	@Override
+	public Object visitSimpleType(SimpleType simpleType, Object arg)
+			throws Exception {
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+
+	@Override
+	public Object visitSizeExpression(SizeExpression sizeExpression, Object arg)
+			throws Exception {
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+
+	@Override
+	public Object visitStringLitExpression(
+			StringLitExpression stringLitExpression, Object arg)
+			throws Exception {
+		stringLitExpression.setType(stringType);
+		return stringType;
+	}
+
+	/**
+	 * if ! and boolean, then boolean else if - and int, then int else error
+	 */
+	@Override
+	public Object visitUnaryExpression(UnaryExpression unaryExpression,
+			Object arg) throws Exception {
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+
+	@Override
+	public Object visitUndeclaredType(UndeclaredType undeclaredType, Object arg)
+			throws Exception {
+		throw new UnsupportedOperationException(
+				"undeclared types not supported");
+	}
+
+	@Override
+	public Object visitValueExpression(ValueExpression valueExpression,
+			Object arg) throws Exception {
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+
+	/**
+	 * check that this variable has not already been declared in the same scope.
+	 */
+	@Override
+	public Object visitVarDec(VarDec varDec, Object arg) throws Exception {
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+
+	/**
+	 * All checking will be done in the children since grammar ensures that the
+	 * rangeExpression is a rangeExpression.
+	 */
+	@Override
+	public Object visitWhileRangeStatement(
+			WhileRangeStatement whileRangeStatement, Object arg)
+			throws Exception {
+		throw new UnsupportedOperationException("not yet implemented");
+
+	}
+
+	@Override
+	public Object visitWhileStarStatement(
+			WhileStarStatement whileStarStatement, Object arg) throws Exception {
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+
+	@Override
+	public Object visitWhileStatement(WhileStatement whileStatement, Object arg)
+			throws Exception {
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+
+}
--- src/cop5555sp15/ast/UndeclaredType.java
+++ src/cop5555sp15/ast/UndeclaredType.java
@@ -1,31 +1,26 @@
 package cop5555sp15.ast;
 
- 
-
 import cop5555sp15.TokenStream.Token;
-
- 
 
 public class UndeclaredType extends Type {
 
- 
+	public UndeclaredType(Token firstToken) {
+		super(firstToken);
+	}
 
-       public UndeclaredType(Token firstToken ) {
+	@Override
+	public Object visit(ASTVisitor v, Object arg) throws Exception {
+		return v.visitUndeclaredType(this, arg);
+	}
 
-             super ( firstToken );
+	@Override
+	String getJVMType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-       }
 
- 
-
-       @Override
-
-       public Object visit(ASTVisitor v , Object arg ) throws Exception {
-
-             return v.visitUndeclaredType( this , arg );
-
-       }
-
- 
+	
+	
 
 }
